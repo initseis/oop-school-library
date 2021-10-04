b37a233b33281300622cd944dbb977bf68b78406
Kevin <kevinsevinche@gmail.com>
2021-10-04 13:18:32 -0500

    Update person class

--- person.rb
+++ person.rb
@@ -1,13 +1,13 @@
 class Person
+  attr_accessor :name, :age
+  attr_reader :id
+
   def initialize(age, parent_permission = true, name = 'Unknown')
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
-
-  attr_accessor :name, :age
-  attr_reader :id
 
   private
 
