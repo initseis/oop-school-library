043f37c8f1a8ee87f2381ce4f38916c938360b07
Kevin <kevinsevinche@gmail.com>
2021-10-12 16:24:22 -0500

    Handle errors for not found json files

--- create_rental.rb
+++ create_rental.rb
@@ -1,23 +1,26 @@
 class CreateRental
-  def create_rental(books, people)
+  def create_rental(books, people, rentals)
     puts 'Select a book from the following list by number (not id)'
     books.each.with_index do |b, index|
-      puts "#{index}) Title: #{b.title}, Author: #{b.author}"
+      puts "#{index}) Title: #{b['title']}, Author: #{b['author']}"
     end
     book_index = gets.chomp
     puts ''
     puts 'Select a person from the following list by number (not id)'
     people.each.with_index do |p, index|
-      if p.instance_of?(Student)
-        puts "#{index}) [Student] Name: #{p.name}, ID: #{p.id}, Age: #{p.age}"
+      if p['specialization']
+        puts "#{index}) [Teacher] Name: #{p['name']}, ID: #{p['id']}, Age: #{p['age']}"
       else
-        puts "#{index}) [Teacher] Name: #{p.name}, ID: #{p.id}, Age: #{p.age}"
+        puts "#{index}) [Student] Name: #{p['name']}, ID: #{p['id']}, Age: #{p['age']}"
       end
     end
     person_index = gets.chomp
     print 'Date: '
     date = gets.chomp
     Rental.new(date, people[person_index.to_i], books[book_index.to_i])
+    rental = { 'date' => date, 'book' => books[book_index.to_i]['title'], 'author' => books[book_index.to_i]['author'],
+               'id_person' => people[person_index.to_i]['id'] }
+    rentals.push(rental)
     puts ''
     puts "Date: #{date}"
     puts 'Rental created successfully'
